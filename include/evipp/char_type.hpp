60070e28117bc2baa0954c41aaa4800b13e6d609
Vinzenz Feenstra <contact@virtuosic-bytes.com>
2012-02-29 08:54:13 +0100

    added evipp/char_type.hpp evipp/string/char_type.hpp

---
+++ include/evipp/char_type.hpp
@@ -0,0 +1,11 @@
+#ifndef GUARD_EVIPP_CHAR_TYPE_HPP_INCLUDED
+#define GUARD_EVIPP_CHAR_TYPE_HPP_INCLUDED
+
+namespace evipp {
+
+typedef char char_type;
+
+}
+
+#endif //GUARD_EVIPP_CHAR_TYPE_HPP_INCLUDED
+
---
+++ include/evipp/string/char_type.hpp
@@ -0,0 +1,14 @@
+#ifndef GUARD_EVIPP_STRING_CHAR_TYPE_HPP_INCLUDED
+#define GUARD_EVIPP_STRING_CHAR_TYPE_HPP_INCLUDED
+
+#include <evipp/string/char_type.hpp>
+
+namespace evipp {
+namespace string {
+
+typedef evipp::string::char_type char_type;
+
+}}
+
+#endif //GUARD_EVIPP_STRING_CHAR_TYPE_HPP_INCLUDED
+
