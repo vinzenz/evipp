ed8851630945ca9bd822cef95d481107ee9a6866
Vinzenz Feenstra <contact@virtuosic-bytes.com>
2012-03-03 08:20:01 +0100

    Added boost library 'auto' detection

--- build/premake4.lua
+++ build/premake4.lua
@@ -1,3 +1,10 @@
+
+local BOOST_LIB_DIR = os.findlib("boost_syste")
+
+if BOOST_LIB_DIR == nil then
+    print( "\nWARNING: Couldn't find required boost libraries directory.\n" )
+end
+
 solution "evipp"
    configurations { "Debug", "Release" }
  
@@ -15,14 +22,25 @@ solution "evipp"
          defines { "NDEBUG" }
          flags { "Optimize" }    
 
-   project "evipp_test"
-	kind "ConsoleApp"
-	language "C++"
-	files { "../unit/**.cpp", "../unit/**.hpp" }
-	links { "evipp" }
-	includedirs { "../include", "../utils/libs/tut" }
-	configuration { "windows" }
-		postbuildcommands { "evipp_test regression" }
-	configuration { "not windows" }
-		postbuildcommands { "./evipp_test regression" }
-		
+    project "evipp_test"
+        kind "ConsoleApp"
+        language "C++"
+        files { "../unit/**.cpp", "../unit/**.hpp" }
+        links { "evipp" }
+        includedirs { "../include", "../utils/libs/tut" }
+        libdirs { BOOST_LIB_DIR }
+
+        configuration { "windows" }
+            postbuildcommands { "evipp_test regression" }
+        configuration { "not windows" }
+            postbuildcommands { "./evipp_test regression" }
+    
+
+        configuration "Debug"
+            defines { "DEBUG" }
+            flags { "Symbols" }
+ 
+        configuration "Release"
+            defines { "NDEBUG" }
+            flags { "Optimize" }    
+
