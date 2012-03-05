e43759c4a3fdf17d56db20ee548868853e6fbfa8
Vinzenz Feenstra <contact@virtuosic-bytes.com>
2012-03-05 08:57:32 +0100

    Doxygen documentation added

--- include/evipp/string/detail/type.hpp
+++ include/evipp/string/detail/type.hpp
@@ -12,11 +12,16 @@ namespace evipp {
 namespace string {
 namespace detail {
 
+/// @brief Implementation of the basic string class template
+///
+/// @tparam CharType	type to be used as character element
+/// @tparam CharTraits  character traits to be used as implementation for the string class
 template< 
 	typename CharType,
 	typename CharTraits // = evipp::string::detail::traits<CharType>
 >
 class type 
+	// will implement operators <, != for us
 	: boost::totally_ordered<
 		type<
 			CharType, 
@@ -29,9 +34,15 @@ class type
 	> 
 {
 public:
+	
+	/// @brief string traits
 	typedef CharTraits			traits;
+	/// @brief character type
 	typedef CharType			char_type;
+	/// @brief string value representation type
 	typedef char_type const *	const_pointer;
+
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
 private:
 	static char_type empty_string[1];
 private:
@@ -44,38 +55,83 @@ private:
 		size_type const character_count;
 	};
 	std::shared_ptr<data const> value_;
+#endif // DOXYGEN_SHOULD_SKIP_THIS
 
 public:
+	
+	/// @brief Default constructor
 	type();
 
+   
+	/// @brief Constructor
+	///
+	/// @param value	pointer to a string which does not need to be null terminated
+	/// @param length   length of the buffer in elements
 	type( 
 		const_pointer value, 
 		string::size_type length );
 
+	/// @brief Constructor
+	///
+	/// @param value	pointer to a null terminated string
 	type( 
 		const_pointer value );
 	
+	/// @brief Copy constructor
+	///
+	/// @param other The object to be copied
 	type( 
 		type const & other );
 	
+	/// @brief Combine constructor
+	///
+	/// By using this constructor you can initialize 
+	/// this string as combination of two strings
+	///
+	/// @param lhs  string where rhs gets appended to
+	/// @param rhs  string which will be appended to lhs
 	type( 
 		type const & lhs, 
 		type const & rhs );
 
+	/// @brief Move constructor
 	type( 
 		type && other );
 
+	/// @brief assignment operator
+	///
+	/// @param o	object to be copied
+	///
+	/// @return	 *this
 	type &  
 		operator=(
 			type o);
 	
+	
+	/// @brief returns the number of characters in the string (Note: it's not necessarily the count of elements)
+	///
+	/// @return number of characters in the string
 	size_type
 		character_count() const;
 
+	/// @brief 
+	///
+	/// @return 
 	size_type 
 		size() const;
+		
+	/// @brief returns a zero terminated sequence of characters
+	///
+	///	Example: if char_type is char and the string is empty it would return an equivalent to ""
+	///
+	/// @return zero terminated sequence of characters
 	const_pointer
 		c_str() const;
+
+
+	/// @brief returns true if the string is empty
+	///
+	/// @return true if the string is empty
 	bool 
 		empty() const;
 
@@ -105,12 +161,26 @@ public:
 			rhs.value_->value.data(),
 			rhs.value_->value.size() ) == 1;
 	}
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
 private:	
+	
+	/// @brief strlen for this class
+	///
+	/// @param value a pointer to a zero terminated sequence of char_type
+	///
+	/// @return size in elements
 	static 
 	size_type 
 		len( 
 			const_pointer value );
 	
+	
+	/// @brief Combines two strings
+	///
+	/// @param lhs	the string which will be on the left
+	/// @param rhs	the string which will be on the right
+	///
+	/// @return combined string in internal representation
 	static
 	auto
 		combine( 
@@ -118,6 +188,11 @@ private:
 			data const & rhs 
 		) -> std::shared_ptr<data>;
 
+	
+	/// @brief Converts from a source encoding to the target encoding
+	///
+	/// @tparam OtherType 	type with different type traits
+	/// @param other		an instance of OtherType to convert from
 	template< 
 		typename OtherType
 	>
@@ -125,6 +200,7 @@ private:
 	void
 		convert(
 			OtherType const & other );
+#endif //DOXYGEN_SHOULD_SKIP_THIS
 };
 
 }}}
