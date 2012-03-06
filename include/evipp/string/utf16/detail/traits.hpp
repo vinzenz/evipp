016cd9cce5762a82599015d0f1dc082f12f4e7da
Vinzenz Feenstra <vinzenz.feenstra@avg.com>
2012-03-06 22:41:15 +0100

    Added headers for utf16/utf32 traits

---
+++ include/evipp/string/utf16/detail/traits.hpp
@@ -0,0 +1,13 @@
+#ifndef GUARD_STRING_UTF16_DETAIL_TRAITS_HPP_INCLUDED
+#define GUARD_STRING_UTF16_DETAIL_TRAITS_HPP_INCLUDED
+
+namespace evipp {
+namespace string {
+namespace utf16 {
+namespace detail {
+
+
+}}}}
+
+#endif //GUARD_STRING_UTF16_DETAIL_TRAITS_HPP_INCLUDED
+
---
+++ include/evipp/string/utf32/detail/traits.hpp
@@ -0,0 +1,13 @@
+#ifndef GUARD_STRING_UTF32_DETAIL_TRAITS_HPP_INCLUDED
+#define GUARD_STRING_UTF32_DETAIL_TRAITS_HPP_INCLUDED
+
+namespace evipp {
+namespace string {
+namespace utf32 {
+namespace detail {
+
+
+}}}}
+
+#endif //GUARD_STRING_UTF32_DETAIL_TRAITS_HPP_INCLUDED
+
