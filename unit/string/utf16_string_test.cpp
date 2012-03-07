feaecaa1f9069c1ebdd3c783079dfc93158dbfe7
Vinzenz Feenstra <contact@virtuosic-bytes.com>
2012-03-07 08:52:06 +0100

    Added utf16 and utf32 tests

---
+++ unit/string/utf16_string_test.cpp
@@ -0,0 +1,36 @@
+#include "../evipp_test.hpp"
+#include <evipp/string/utf16/type.hpp>
+#include <evipp/string/utf16/type_impl.hpp>
+
+struct utf16_string_data {
+	evipp::string::utf16::type str;
+};
+
+EVIPP_TEST_GROUP_DEF(utf16_string_data, "evipp::string::utf16")
+
+EVIPP_TEST_DEF(1)
+{
+	ensure( "str.size() == 0", str.size() == 0 );
+	ensure( "str.empty() == true", str.size() == 0 );
+}
+
+EVIPP_TEST_DEF(2)
+{
+	str = evipp::string::utf16::type(u"test text");
+	ensure( "str.size() == 9 where str = 'test text'", str.size() == 9 );
+}
+
+EVIPP_TEST_DEF(3)
+{
+	str = evipp::string::utf16::type(u"");
+	ensure( "empty after assigning evipp::string(\"\")", str.empty() );
+}
+
+EVIPP_TEST_DEF(4)
+{
+	str = u"test text";
+	ensure( "str.size() == 9 where str = 'test text'", str.size() == 9 );
+}
+
+EVIPP_TEST_GROUP_END
+
