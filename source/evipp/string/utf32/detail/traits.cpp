6f2aae0d6e080a144ca5146d27bc445e4c9625e0
Vinzenz Feenstra <contact@virtuosic-bytes.com>
2012-03-07 09:31:34 +0100

    Added traits cpp for utf32 and utf16

---
+++ source/evipp/string/utf32/detail/traits.cpp
@@ -0,0 +1,61 @@
+#include <evipp/string/utf32/detail/traits.hpp>
+
+#include <cstring>
+#include <cassert>
+
+namespace evipp {
+namespace string {
+namespace utf32 {
+namespace detail {
+
+traits::size_type
+traits::len(
+	const_pointer value )
+{
+	size_type count = 0;
+	for(; value && *value; ++value, ++count )
+	{
+		//empty
+	}
+	return count;
+}
+
+traits::size_type
+traits::character_count(
+	const_pointer value,
+	size_type element_count )
+{
+	//TODO: implement character counting for UTF-32
+	return element_count;
+}
+
+int
+traits::compare(
+	const_pointer lhs,
+	size_type lhs_element_count,
+	const_pointer rhs,
+	size_type rhs_element_count )
+{
+	if( 
+			lhs_element_count 
+		> 	rhs_element_count ) 
+	{
+		return -1;
+	}
+
+	if( 
+			lhs_element_count 
+		< 	rhs_element_count ) 
+	{
+		return 1;
+	}
+	
+	//Precondition: lhs_element_count == rhs_element_count
+	assert( lhs_element_count == rhs_element_count );
+
+	//TODO: implement utf32 string comparison
+	return std::memcmp( lhs, rhs, rhs_element_count * sizeof(*lhs) );
+}
+
+}}}}
+
