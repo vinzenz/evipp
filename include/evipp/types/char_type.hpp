4f5fa7b000e3f6448e3eaad9326bbb2c1f70be9d
Vinzenz Feenstra <contact@virtuosic-bytes.com>
2012-03-11 12:26:28 +0100

    Making evipp::types::char_type compiler dependent -> Build dependent

--- include/evipp/types/char_type.hpp
+++ include/evipp/types/char_type.hpp
@@ -1,10 +1,12 @@
 #ifndef GUARD_EVIPP_TYPES_CHAR_TYPE_HPP_INCLUDED
 #define GUARD_EVIPP_TYPES_CHAR_TYPE_HPP_INCLUDED
 
+#include <evipp/config/compiler.hpp>
+
 namespace evipp {
 namespace types {
 
-typedef char char_type;
+typedef evipp::config::compiler::char_type char_type;
 
 }}
 
